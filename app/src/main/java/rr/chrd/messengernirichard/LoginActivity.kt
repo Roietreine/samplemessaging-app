978fd7edd7d27cab3dee3d1c718e912034a8a84a
Roietreine <richardrejano52296@gmail.com>
2022-04-28 16:18:18 +0800

    register is done

--- app/src/main/java/rr/chrd/messengernirichard/LoginActivity.kt
+++ app/src/main/java/rr/chrd/messengernirichard/LoginActivity.kt
@@ -2,10 +2,26 @@ package rr.chrd.messengernirichard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import rr.chrd.messengernirichard.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit  var _binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        _binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(_binding.root)
+        _binding.loginButton.setOnClickListener {
+            loginFun()
+        }
+        _binding.backRegister.setOnClickListener {
+            finish()
+        }
+    }
+    private fun loginFun() {
+        val email = _binding.loginEmail.text.toString()
+        val password = _binding.loginPassword.text.toString()
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
     }
 }
--- app/src/main/java/rr/chrd/messengernirichard/MainActivity.kt
+++ app/src/main/java/rr/chrd/messengernirichard/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 import rr.chrd.messengernirichard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,24 +16,30 @@ class MainActivity : AppCompatActivity() {
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
-
+        registerButton()
+        showLogin()
+    }
+    private fun showLogin() {
+        _binding.textAccount.setOnClickListener {
+            val intent = Intent (this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    private fun registerButton() {
         _binding.registerButton.setOnClickListener {
-
-            val email = _binding.editUsername.text.toString()
+            val email = _binding.editEmail.text.toString()
             val password = _binding.editPassword.text.toString()
 
-            Log.d("Main Activity", "emails is: " + email)
-            Log.d("Main Activity", "password:$password")
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this,"Please enter your desire e-mail and password",Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email,password)
+                .addOnSuccessListener{
+                    Log.d("Main","Succesfully created user with uid: ${it.user!!.uid}")
+                }
+                .addOnFailureListener{
+                Toast.makeText(this,"Please enter a correct email format",Toast.LENGTH_LONG).show()}
         }
-        _binding.registerButton.setOnClickListener {
-            Log.d("MainActivity", "Try to show login activity")
-            startFun()
-        }
-
-    }
-
-    private fun startFun() {
-        val intent = Intent (this,LoginActivity::class.java)
-        startActivity(intent)
     }
 }
